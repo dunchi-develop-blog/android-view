51be32aea28e7a3a7a47b78ced44b632621a9763
LDongGyu <ehdrbchd@naver.com>
2020-12-08 20:43:15 +0900

    feat: LinearLayout 예제 추가

---
+++ app/src/main/java/com/dunchi/android_view/general/LinearLayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.dunchi.android_view.general
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dunchi.android_view.R
+
+class LinearLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
