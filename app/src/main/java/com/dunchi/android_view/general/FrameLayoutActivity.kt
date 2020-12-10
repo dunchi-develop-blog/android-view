7390627e4b675fd2480c099694458a2e36c761af
LDongGyu <ehdrbchd@naver.com>
2020-12-10 19:14:03 +0900

    feat: FrameLayout 예제 추가

---
+++ app/src/main/java/com/dunchi/android_view/general/FrameLayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.dunchi.android_view.general
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dunchi.android_view.R
+
+class FrameLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
