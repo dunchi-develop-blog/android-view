97a7395fd8be73efee15c4517837e14f1f19402a
LDongGyu <ehdrbchd@naver.com>
2020-12-12 17:01:27 +0900

    feat: MotionLayout 예제 추가

--- app/src/main/java/com/dunchi/android_view/MainActivity.kt
+++ app/src/main/java/com/dunchi/android_view/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.dunchi.android_view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dunchi.android_view.general.MotionLayoutActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layoutBtn.setOnClickListener {
+            startActivity(Intent(this,MotionLayoutActivity::class.java))
+        }
     }
 }
