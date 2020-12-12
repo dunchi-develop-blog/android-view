77a8025f09c230977cb26768538f3d8b88f99072
LDongGyu <ehdrbchd@naver.com>
2020-12-12 23:52:54 +0900

    feat: TableLayout 예제 추가

---
+++ app/src/main/java/com/dunchi/android_view/general/TableLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.dunchi.android_view.general
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_layout)
+    }
+}
