24c6b77b9a0db3a84f982a854e2559a8c60fb07f
LDongGyu <ehdrbchd@naver.com>
2020-12-13 17:24:27 +0900

    refactor: AbsoluteLayoutActivity 이름 변경

--- app/src/main/java/com/dunchi/android_view/general/AbsoluteLayoutActivity.kt
+++ app/src/main/java/com/dunchi/android_view/general/AbsoluteLayoutActivity.kt
@@ -2,8 +2,9 @@ package com.dunchi.android_view.general
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dunchi.android_view.R
 
-class AbsoluteLayout : AppCompatActivity() {
+class AbsoluteLayoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
