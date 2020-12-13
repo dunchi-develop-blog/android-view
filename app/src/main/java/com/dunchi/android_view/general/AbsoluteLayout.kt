92425427d71324f7d3925aadb60542ade1b33bc5
LDongGyu <ehdrbchd@naver.com>
2020-12-13 17:01:19 +0900

    feat: AbsoluteLayout 예제 추가

---
+++ app/src/main/java/com/dunchi/android_view/general/AbsoluteLayout.kt
@@ -0,0 +1,12 @@
+package com.dunchi.android_view.general
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AbsoluteLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_absolute_layout)
+    }
+}
