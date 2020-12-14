ea8392534fc74199be3c2a38c1a6e1e7481c73c0
LDongGyu <ehdrbchd@naver.com>
2020-12-14 22:49:57 +0900

    feat: listview 예제 activity 추가

---
+++ app/src/main/java/com/dunchi/android_view/adapter/listview/ListViewActivity.kt
@@ -0,0 +1,13 @@
+package com.dunchi.android_view.adapter.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dunchi.android_view.R
+
+class ListViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+    }
+}
