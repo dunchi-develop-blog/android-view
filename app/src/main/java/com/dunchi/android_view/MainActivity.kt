91e169b7c2dd8c953051aa7a688d4071a66b4e17
LDongGyu <ehdrbchd@naver.com>
2020-12-26 13:02:25 +0900

    feat: spinner 예제 추가

--- app/src/main/java/com/dunchi/android_view/MainActivity.kt
+++ app/src/main/java/com/dunchi/android_view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dunchi.android_view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.dunchi.android_view.general.MotionLayoutActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         layoutBtn.setOnClickListener {
             startActivity(Intent(this,MotionLayoutActivity::class.java))
         }
+
+        var spinnerItems = arrayOf("은비","쿠라","혜원","예나","채연","채원","민주","나코","토미","유리","유진","원영")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
+        spinner.adapter = adapter
     }
 }
